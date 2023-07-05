07c4d6968b1b787caac84624b42afd0b8aff5d87
Al-Amin Ifty <ifty.alamin54@gmail.com>
2023-07-05 23:53:43 +0600

    Update svm.py

--- svm.py
+++ svm.py
@@ -4,12 +4,12 @@ classifier = SVC(kernel ='rbf', random_state = 0)
 # training set in x, y axis
 classifier.fit(x_train, y_train)
 
-#Sigmoid Kernel
+#Sigmoid Kernel Function
 from sklearn.svm import SVC
 classifier = SVC(kernel ='sigmoid')
 classifier.fit(x_train, y_train) # training set in x, y axis
 
-#Polynomial Kernel
+#Polynomial Kernel Function
 from sklearn.svm import SVC
 classifier = SVC(kernel ='poly', degree = 4)
 classifier.fit(x_train, y_train) # training set in x, y axis
